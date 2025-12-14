2b2061e0faef3b2a917eef12bb39b3babdd61cd2
RogelioGio <giotalingdan@gmail.com>
2025-12-14 19:53:00 +0800

    Final Submission - Thesis Completion

---
+++ app/Models/Enrollment.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Enrollment extends Model
+{
+    /** @use HasFactory<\Database\Factories\EnrollmentFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'course_id',
+        'enroller_id',
+        //'enrolled', 'ongoing', 'finished', 'late_finish', 'due-soon', 'past-due', 'failed'
+        'enrollment_status',
+        'due_soon',
+        'start_date',
+        'end_date',
+    ];
+
+    protected function casts(): Array{
+        return [
+            'due_soon' => 'boolean',
+            'allow_late' => 'boolean',
+        ];
+    }
+
+    public function enrolledUser(): BelongsTo{
+        return $this->belongsTo(UserInfos::class, 'user_id', 'id');
+    }
+
+    public function course(): BelongsTo{
+        return $this->belongsTo(Course::class);
+    }
+
+    public function enroller(): BelongsTo{
+        return $this->belongsTo(UserInfos::class, 'enroller_id', 'id');
+    }
+}
